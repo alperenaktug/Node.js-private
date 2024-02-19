30628e97a1e437c745342bd66e5b0df0a0fe0b39
Alperen Aktuğ <aktugalperen9@gmail.com>
2024-02-20 00:05:49 +0300

    fs

---
+++ 9-File-system-module.js
@@ -0,0 +1,34 @@
+let http = require("http");
+let fs = require("fs");
+
+let server = http.createServer((request, response) => {
+  if (request.url == "/") {
+    fs.readFile("index.html", (error, html) => {
+      response.writeHead(200, { "Content-Type": "text/html" });
+      response.write(html);
+      response.end();
+    });
+  } else if (request.url == "/blogs") {
+    fs.readFile("blogs.html", (error, html) => {
+      response.writeHead(200, { "Content-Type": "text/html" });
+      response.write(html);
+      response.end();
+    });
+  } else if (request.url == "/product") {
+    fs.readFile("product.html", (error, html) => {
+      response.writeHead(200, { "Content-Type": "text/html" });
+      response.write(html);
+      response.end();
+    });
+  } else {
+    fs.readFile("404.html", (error, html) => {
+      response.writeHead(200, { "Content-Type": "text/html" });
+      response.write(html);
+      response.end();
+    });
+  }
+});
+
+server.listen(3002);
+
+console.log("node js server at port 3002");
